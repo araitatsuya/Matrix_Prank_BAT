4b857dcff4376a83094607873285602a379a4c2f
Tatsuya Arai <araitatsuyaj@gmail.com>
2016-05-29 18:23:37 -0500

    Add files via upload

---
+++ Matrix.bat
@@ -0,0 +1,8 @@
+@echo off
+pause
+color 0A
+mode 1000
+
+:matrix 
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto matrix
